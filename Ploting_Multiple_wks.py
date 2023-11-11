f4454768c11b5d589180c9cec8b49fdc31227a93
P <117578336+prathameshnium@users.noreply.github.com>
2023-11-11 17:58:47 +0530

    Update Ploting_Multiple_wks.py
    Legends added

--- Ploting_Multiple_wks.py
+++ Ploting_Multiple_wks.py
@@ -1,15 +1,38 @@
 import originpro as op
-gp = op.new_graph()
- 
+gp = op.new_graph() 
 gl = gp[0]
-  
-   
+def generate_legend_text(size):
+    lgnd_template = '\\l({}) %({}, @ws)\n'
+    result = ''
+
+    for x in range(1, size + 1):
+        lgnd_text = lgnd_template.format(x, x)
+        result += lgnd_text
+
+    return result
+ 
 
 
 for wb_index in range(40):
     wks = op.find_sheet('w',wb_index)
     print(wks)
-    gl.add_plot(wks, 1, 0)
+
+    plot=gl.add_plot(wks, 'tand','Temperature')
+    #gl.group()
+
+    plot.colormap='Fire'
+    plot.symbol_size=4
+
+    lgnd = gp[0].label('Legend')
+    legend_text = generate_legend_text(40)
+
+    lgnd.text=legend_text
+
 
     
+    
+    
+lgnd.set_int('left',4900)
+lgnd.set_int('top',100)    
+plot.colormap='Candy'    
 gl.rescale()
