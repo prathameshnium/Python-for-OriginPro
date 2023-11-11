68984278b3f0b71fa72ddce81e4ec3ec78b4bbd6
P <117578336+prathameshnium@users.noreply.github.com>
2023-11-11 22:31:20 +0530

    Update Ploting_Multiple_wks.py
    user input added

--- Ploting_Multiple_wks.py
+++ Ploting_Multiple_wks.py
@@ -10,21 +10,25 @@ def generate_legend_text(size):
         result += lgnd_text
 
     return result
- 
+starting=int(input("Enter the starting index for the plot , origin indexig start with 0 : "))
+ending=int(input("Enter the Final index for the plot")) 
+steps=int(input("Enter the step fot index for the plot")) 
 
 
-for wb_index in range(40):
+for wb_index in range(starting,ending,steps):
     wks = op.find_sheet('w',wb_index)
     print(wks)
 
-    plot=gl.add_plot(wks, 'tand','Temperature')
+    plot=gl.add_plot(wks, 'M2','Temperature')
     #gl.group()
 
     plot.colormap='Fire'
     plot.symbol_size=4
+    #lgnd = gl.label('Legend')
+
 
     lgnd = gp[0].label('Legend')
-    legend_text = generate_legend_text(40)
+    legend_text = generate_legend_text(len(range(starting,ending,steps)))
 
     lgnd.text=legend_text
 
