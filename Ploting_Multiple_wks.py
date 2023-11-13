b584e74b3e4bd738e09c854be1c3b09f90a6b9b8
P <117578336+prathameshnium@users.noreply.github.com>
2023-11-13 14:22:30 +0530

    Update Ploting_Multiple_wks.py
    For loop ending modified

--- Ploting_Multiple_wks.py
+++ Ploting_Multiple_wks.py
@@ -1,5 +1,5 @@
 import originpro as op
-import seaborn as sns
+#import seaborn as sns
 
 gp = op.new_graph() 
 gl = gp[0]
@@ -17,24 +17,24 @@ ending=int(input("Enter the Final index for the plot"))
 steps=int(input("Enter the step fot index for the plot")) 
 
 
-for wb_index in range(starting,ending,steps):
+for wb_index in range(starting,ending+1,steps):
     wks = op.find_sheet('w',wb_index)
     print(wks)
 
-    plot=gl.add_plot(wks, 'M2','Temperature')
+    plot=gl.add_plot(wks, 'E1','Temperature')
     #gl.group()
 
     #plot.colormap='Fire'
-    pal =  sns.color_palette("Paired")# creates a colour pallete
+    #pal =  sns.color_palette("Paired")# creates a colour pallete
 
-    plot.color=str(pal.as_hex()[wb_index])#convert the pallet to hex , and select the iterating variable
+    #plot.color=str(pal.as_hex()[wb_index])#convert the pallet to hex , and select the iterating variable
 
     plot.symbol_size=4
     #lgnd = gl.label('Legend')
 
 
     lgnd = gp[0].label('Legend')
-    legend_text = generate_legend_text(len(range(starting,ending,steps)))
+    legend_text = generate_legend_text(len(range(starting,ending+1,steps)))
     
 
 
@@ -46,5 +46,5 @@ for wb_index in range(starting,ending,steps):
     
 lgnd.set_int('left',4900)
 lgnd.set_int('top',100)    
-plot.colormap='Candy'    
+#plot.colormap='Candy'    
 gl.rescale()
