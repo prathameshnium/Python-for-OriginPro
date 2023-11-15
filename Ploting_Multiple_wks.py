c761e8271fe2c4526f729c2f040eef5ed4707e6b
P <117578336+prathameshnium@users.noreply.github.com>
2023-11-16 00:39:31 +0530

    Update Ploting_Multiple_wks.py
    Option to plot multiple columns of varoius workbooks added

--- Ploting_Multiple_wks.py
+++ Ploting_Multiple_wks.py
@@ -17,35 +17,30 @@ ending=int(input("Enter the Final index for the plot"))
 steps=int(input("Enter the step fot index for the plot")) 
 
 
-Wb_List=range(starting,ending+1,steps)
-#Wb_List=[0,20,5,25,10,30,15,35,19,39]
-
+#Wb_List=range(starting,ending+1,steps)
+Wb_List=[1]
+#enter the list list of workbook or uncomment the range
 
 
 for wb_index in Wb_List:
     wks = op.find_sheet('w',wb_index)
     print(wks)
     
-    #Commonly used Col_names "Frequency	Cp	G(1/Rp)	E1	E2	Z1	Z2	M1	M2	tand	sigma"
+    #Commonly used Col_names "Frequency	Cp	G(1/Rp)	E1	E2	Z1	Z2	M1	M2	tand	sigma" "Field/Freq"
 
-    plot=gl.add_plot(wks, 'M2','M1')
-    #gl.group()
-
-    #plot.colormap='Fire'
-    #pal =  sns.color_palette("Paired")# creates a colour pallete
-
-    #plot.color=str(pal.as_hex()[wb_index])#convert the pallet to hex , and select the iterating variable
-
-    plot.symbol_size=4
-    #lgnd = gl.label('Legend')
-
-
-    lgnd = gp[0].label('Legend')
-    legend_text = generate_legend_text(len(Wb_List))
     
+    for Col_Index in [500.0,1000.0,5000.0,10000.0,50000.0,100000.0,500000.0,1000000.0]: # column names in above selectd workbooks
+        plot=gl.add_plot(wks,str(Col_Index),'Field/Freq')
+        #gl.group()
+        plot.symbol_size=4
+        #plot.colormap='Fire'
+        #pal =  sns.color_palette("Paired")# creates a colour pallete
+        #plot.color=str(pal.as_hex()[wb_index])#convert the pallet to hex , and select the iterating variable
+        #lgnd = gl.label('Legend')
+        lgnd = gp[0].label('Legend')
+        legend_text = generate_legend_text(len(Wb_List))
+        lgnd.text=legend_text
 
-
-    lgnd.text=legend_text
 
 
     
