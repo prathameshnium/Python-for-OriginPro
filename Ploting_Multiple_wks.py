2154670ad6397a2c191a89febf405f2263cddf7f
P <117578336+prathameshnium@users.noreply.github.com>
2023-11-23 00:27:15 +0530

    Update Ploting_Multiple_wks.py
    If else added (Kten)

--- Ploting_Multiple_wks.py
+++ Ploting_Multiple_wks.py
@@ -1,9 +1,30 @@
 # Prg For Ploting Multiple Workbooks with option to specify Wks and Columns
-#Updated On : 20 Nov 23
+#Updated On : 23 Nov 23
 import originpro as op
-import seaborn as sns
+#import seaborn as sns
 #import sys
 #sys.path.append("C:\\Users\\Admin\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages")
+
+
+
+
+# Selecting Workbook : enter the list list of workbook or uncomment the range
+
+Input_Style = 1 # 0 means Default range , 1 means User Difined List 
+
+if Input_Style == 0:
+    starting=int(input("Enter the starting index for the plot , origin indexig start with 0 : "))
+    ending=int(input("Enter the Final index for the plot : ")) 
+    steps=int(input("Enter the step fot index for the plot : ")) 
+    Wb_List=range(starting,ending+1,steps) # For Multiple Workbook in a range
+    print("Range selected with , Starting: "+str(starting)+" Ending :"+str(ending)+" Steps"+str(steps))
+elif Input_Style == 1:
+    Wb_List=[1,2,10] # For Multiple Workbook by specifing the list
+    print("List Selected : "+str(Wb_List))
+else:
+    print('Error in Input Style for Worksheets to plot')
+
+
 gp = op.new_graph() 
 gl = gp[0]
 def generate_legend_text(size):
@@ -15,13 +36,8 @@ def generate_legend_text(size):
         result += lgnd_text
 
     return result
-starting=int(input("Enter the starting index for the plot , origin indexig start with 0 : "))
-ending=int(input("Enter the Final index for the plot : ")) 
-steps=int(input("Enter the step fot index for the plot : ")) 
 
-# Selecting Workbook : enter the list list of workbook or uncomment the range
-Wb_List=range(starting,ending+1,steps) # For Multiple Workbook in a range
-#Wb_List=[1] # For Multiple Workbook by specifing the list
+
 
 
 for wb_index in Wb_List:
@@ -32,23 +48,25 @@ for wb_index in Wb_List:
     Commonly used Column names 
     "Frequency	Cp	G(1/Rp)	E1	E2	Z1	Z2	M1	M2	tand	sigma" "Field/Freq" "Temperature"
     "[500.0,1000.0,5000.0,10000.0,50000.0,100000.0,500000.0,1000000.0]"
-    
+    Drive Voltage
+    Measured Polarization
+
     """
 
     
-    for Col_Index in ['E1']: # Specify Y , column names in above selectd workbooks 
-        plot=gl.add_plot(wks,str(Col_Index),'Temperature') #  Use this to specify , X here First Y then X axis
+    for Col_Index in ['Measured Polarization']: # Specify Y , column names in above selectd workbooks 
+        plot=gl.add_plot(wks,str(Col_Index),'Drive Voltage') #  Use this to specify , X here First Y then X axis
         #gl.group()
         
         
-        plot.symbol_size=4
+        plot.symbol_size=2
         
         
        # plot.colormap='Fire'
-        pal =  sns.color_palette("Paired")# creates a colour pallete
-        plot.color=str(pal.as_hex()[wb_index])#convert the pallet to hex , and select the iterating variable
+        #pal =  sns.color_palette("Paired")# creates a colour pallete
+        #plot.color=str(pal.as_hex()[wb_index])#convert the pallet to hex , and select the iterating variable
         lgnd = gl.label('Legend')
-        lgnd = gp[0].label('Legend')
+        #lgnd = gp[0].label('Legend')
         legend_text = generate_legend_text(len(Wb_List))
         lgnd.text=legend_text
 
