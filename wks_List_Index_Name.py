88d3f405d1f110aeb17099c47a5a543b0c64813d
P <117578336+prathameshnium@users.noreply.github.com>
2023-11-23 00:59:14 +0530

    Update wks_List_Index_Name.py
    While loop added, no input has to be specified

--- wks_List_Index_Name.py
+++ wks_List_Index_Name.py
@@ -1,30 +1,39 @@
-# Prg for Listing All Workbook in he current File , and save it in a new workbook
-#Updated On : 20 Nov 23
-
 import originpro as op
-#import sys
-#sys.path.append("C:\\Users\\Admin\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages")
 import pandas as pd
 import numpy as np
-
-
-starting=int(input("Enter the starting index for the plot , origin indexig start with 0 : "))
-ending=int(input("Enter the Final index for the plot")) 
-steps=int(input("Enter the step fot index for the plot")) 
+#starting=int(input("Enter the starting index for the plot , origin indexig start with 0 : "))
+#ending=int(input("Enter the Final index for the plot")) 
+#steps=int(input("Enter the step fot index for the plot")) 
 df_wks= pd.DataFrame()
 list1=list()
 list2=list()
+steps=0
 
-for wb_index in range(starting,ending+1,steps):
+
+comments=input("Create List of Worksheets , Just Click on Ok: ")
+
+print("Comment: "+str(comments))
+while True:
+    wb_index=steps
     wks = op.find_sheet('w',wb_index)
-    list1.append(wb_index)
-    list2.append(str(wks))
-
-
+    if str(wks)!= "None":
+        list1.append(wb_index)
+        list2.append(str(wks))
+        print("Index : "+str(wb_index)+"  |  "+str(wks))
+        steps+=1
+    else :
+        break
     
-        
+    
+
+
+#for wb_index in range(starting,ending+1,steps)    
 df_wks['Wb_Index'] =list1
 df_wks['Wb_name'] =list2
+
+print("Workbook List is Saved")
+
+
 
 
 # Create a new worksheet
